d1afadae46a5f12b483c6318090170d2a77c8359
Bea Mungcal <beamungcal@hotmail.com>
2023-05-14 20:12:36 -0400

    ISS index.

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-//will require main fetch function
-
-// URL for retrieving our IPv4 IP address in JSON format.
-// https://api.ipify.org?format=json
-// ip	"99.237.70.155"
-
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss')
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work!", error);
     return;
   }
+  console.log('It worked! Returned IP:', ip);
+ 
+  fetchCoordsByIP(ip, (error, data) => {
+    if (error) {
+      console.log('Error:', error);
+      return;
+    }
+    console.log('Data:', data);
 
-  console.log('It worked! Returned IP:' , ip);
+    fetchISSFlyOverTimes(data, (error, passes) => {
+      if (error) {
+        console.log('Error:', error);
+        return;
+      }
+      console.log('ISS Flyover Times:', passes);
+
+      nextISSTimesForMyLocation((error, passTimes) => {
+        if (error) {
+        return console.log("It didn't work!", error);
+      }
+
+        const printPassTimes = function(passTimes) {
+          for (const pass of passTimes) {
+            const datetime = new Date(0);
+            datetime.setUTCSeconds(pass.risetime * 1000);
+            const duration = pass.duration;
+            console.log(`Next pass at ${datetime} for ${pass.duration} seconds!`);
+          }
+        }
+        printPassTimes(passTimes);
+      })
+    });   
+  });
 });
-
